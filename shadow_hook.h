cba85d3096abc3b3187160d97788d733c45f3632
meoxjdn <120544758+meoxjdn@users.noreply.github.com>
2026-04-19 14:54:05 +0800

    Update shadow_hook.h

--- shadow_hook.h
+++ shadow_hook.h
@@ -9,6 +9,7 @@
 /* 驱动通信协议 IOCTL 魔法字 */
 #define WUWA_IOCTL_ADD_SHADOW _IOW('W', 1, struct shadow_request)
 #define WUWA_IOCTL_RAW_PATCH  _IOW('W', 2, struct raw_patch_req)
+#define WUWA_IOCTL_CLEAN_ALL  _IO('W', 3)  /* 核爆级内存清理指令 */
 
 /* BRK 硬件断点劫持请求包 */
 struct shadow_request {
@@ -17,7 +18,7 @@ struct shadow_request {
     unsigned long target_vaddr;
     u32 custom_rot[3];
     int is_rot_hook;
-    unsigned long jump_vaddr; /* 【新增】极客流无痕跳转目标地址 */
+    unsigned long jump_vaddr; /* 极客流无痕跳转目标地址 */
 };
 
 /* 强力穿透写入请求包 */
@@ -44,5 +45,6 @@ extern void shadow_fault_exit(void);
 extern int add_shadow(struct shadow_request *req);
 extern int update_shadow_rot(struct shadow_request *req);
 extern int del_shadow(struct shadow_request *req);
+extern void clear_all_shadows(void); /* 新增内存清空接口 */
 
 #endif /* _SHADOW_HOOK_H */
